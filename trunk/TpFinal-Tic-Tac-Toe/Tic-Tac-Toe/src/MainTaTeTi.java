99344b668937f9f4133f887f3ace5fd91f273cc8
vero13 <vero13@2bed87ef-8a8a-bb39-842d-1c8c10e0656e>
2012-06-09 21:47:47 +0000

    Primeros trabajos en el tic tac toe

---
+++ trunk/TpFinal-Tic-Tac-Toe/Tic-Tac-Toe/src/MainTaTeTi.java
@@ -0,0 +1,10 @@
+import controlador.ControladorTaTeTi;
+
+public class MainTaTeTi {
+
+	public static void main(String[] args) {
+		ControladorTaTeTi controlador= new ControladorTaTeTi();
+		controlador.cargarTaTeTi();
+		controlador.jugarTaTeTi();
+	}
+}
---
+++ trunk/TpFinal-Tic-Tac-Toe/Tic-Tac-Toe/src/modelo/jugadores/estrategias/Estrategia.java
@@ -0,0 +1,5 @@
+package modelo.jugadores.estrategias;
+
+public interface Estrategia {
+	public int getJugada();
+}
