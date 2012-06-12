d02ae5f8fb53a811f9cf3799765081522289c68a
vero13 <vero13@2bed87ef-8a8a-bb39-842d-1c8c10e0656e>
2012-06-12 05:16:39 +0000

    Para evitar cuando red neuronal devuelva la misma posicion, verificar que las posiciones pasadas no sean las mismas que en otra iteracion. De ser ese el caso, devuelve la segunda mejor opcion que tiene almacenada sin volver a utilizar la red neuronal.

--- trunk/TpFinal-TaTeTi/TaTeTi/src/modelo/jugadores/estrategias/EstrategiaRedNeuronal.java
+++ trunk/TpFinal-TaTeTi/TaTeTi/src/modelo/jugadores/estrategias/EstrategiaRedNeuronal.java
@@ -1,6 +1,11 @@
 package modelo.jugadores.estrategias;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 import modelo.Constantes;
 import modelo.Ficha;
@@ -11,6 +16,10 @@ public class EstrategiaRedNeuronal extends EstrategiaComputadora {
 
 	private RedNeuronalTaTeTi rn;
 	private Ficha ficha;
+	private double[][] tableroAnterior;
+	private LinkedHashMap<Integer,BigDecimal> jugadas;
+	private int posicion;
+	private int[] jugadasA;
 	
 	public EstrategiaRedNeuronal(Ficha ficha) {
 		rn= new RedNeuronalTaTeTi();
@@ -20,25 +29,65 @@ public class EstrategiaRedNeuronal extends EstrategiaComputadora {
 	
 	@Override
 	public int generarJugada(Tablero tablero) {
-		double[][] inputArray= new double[1][9];
+		double[][] tabl= new double[1][9];
 		double valor= 0.0;
 		for (int i = 0; i < 9; i++) {
 			if(tablero.getCasillero(i) != Ficha.VACIO) {
 				if((tablero.getCasillero(i).compareTo(ficha)) == 0) valor= 1.0;
 				else valor= -1.0;
 			}
-			inputArray[0][i]= valor;
+			tabl[0][i]= valor;
 		}	
-		BigDecimal[] resultados= rn.preguntar(inputArray);
-		return getMejorPosicion(resultados);
+		if(!esTableroAnterior(tabl)) {
+			BigDecimal[] resultados= rn.preguntar(tabl);
+			cargarJugadas(resultados);
+			ordenarJugadas();
+			cargarJugadas2();
+			tableroAnterior= tabl;
+			posicion= 0;
+		} else posicion++;
+		return jugadasA[posicion];
 	}
 
-	//TODO: mejorar!!!!!!!
-	private int getMejorPosicion(BigDecimal[] resultados) {
-		int mejor= 0;
-		for (int i= 0; i < resultados.length; i++) {
-			if(resultados[mejor].compareTo(resultados[i]) < 0) mejor= i;
+	private boolean esTableroAnterior(double[][] tableroIngresado) {
+		if(tableroAnterior == null) return false;
+		boolean iguales= true;
+		for(int i = 0; i < tableroIngresado.length; i++) {
+			if(tableroAnterior[0][i] != tableroIngresado[0][i]) {
+				iguales= false;
+				break;
+			}
 		}
-		return mejor;
+		return iguales;
+	}
+
+	private void cargarJugadas(BigDecimal[] resultados) {
+		jugadas= new LinkedHashMap<Integer, BigDecimal>(9);
+		for (int i = 0; i < resultados.length; i++)
+			jugadas.put(new Integer(i), resultados[i]);		
+	}
+	
+	private void ordenarJugadas() {
+		LinkedHashMap<Integer,BigDecimal> newMap= new LinkedHashMap<Integer,BigDecimal>();
+		ArrayList<BigDecimal> values= new ArrayList<BigDecimal>(jugadas.values());
+		Collections.sort(values);
+		Iterator<BigDecimal> it= values.iterator();
+		BigDecimal tmp= BigDecimal.ZERO;
+		while(it.hasNext()) {
+			tmp = it.next();
+			for(Map.Entry<Integer,BigDecimal> k : jugadas.entrySet()) {
+				if(tmp==k.getValue()) newMap.put(k.getKey(), k.getValue());
+			}
+		}
+	}
+	
+	private void cargarJugadas2() {
+		jugadasA= new int[9];
+		ArrayList<Integer> values= new ArrayList<Integer>(jugadas.keySet());
+		Iterator<Integer> it= values.iterator();
+		int i= 0;
+		for (it= values.iterator(); it.hasNext(); i++) {
+			jugadasA[i]= (it.next()).intValue();
+		}
 	}
 }
