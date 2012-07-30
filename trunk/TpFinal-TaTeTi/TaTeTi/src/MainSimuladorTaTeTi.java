906b601f7fda3463593f7daf3c291a570605ebfa
vero13 <vero13@2bed87ef-8a8a-bb39-842d-1c8c10e0656e>
2012-07-30 04:55:43 +0000

    Simulacion no harcodeada

--- trunk/TpFinal-TaTeTi/TaTeTi/src/MainSimuladorTaTeTi.java
+++ trunk/TpFinal-TaTeTi/TaTeTi/src/MainSimuladorTaTeTi.java
@@ -1,22 +1,56 @@
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.PrintStream;
+import java.util.Scanner;
 
 import controlador.ControladorTaTeTi;
 
 public class MainSimuladorTaTeTi {
 
 	public static void main(String[] args) {
+		//Seleccion contrario y cantidad de partidas
+		System.out.println("--- Simulacion de Partidas Ta-Te-Ti ---");
+		Scanner in= new Scanner(System.in);
+		System.out.println("Elija el rival:");
+		System.out.println("1- Humano");
+		System.out.println("2- Random");
+		System.out.println("3- Procedural");
+		System.out.print("Opcion elegida: ");
+		int rival= in.nextInt();
+		int estrategia;
+		switch (rival) {
+		case 1:
+			estrategia= ControladorTaTeTi.ESTRATEGIA_HUMANO;
+			break;
+		case 2:
+			estrategia= ControladorTaTeTi.ESTRATEGIA_RANDOM;
+			break;
+		case 3:
+			estrategia= ControladorTaTeTi.ESTRATEGIA_PROCEDURAL;
+			break;
+		default:
+			estrategia= ControladorTaTeTi.ESTRATEGIA_RANDOM;
+			break;
+		}
+		
+		System.out.print("Elija la cantidad de partidas: ");
+		int cantPartidas= in.nextInt();
+		System.out.println();
+		System.out.println("Simulacion comenzando...");
+		
 		//Direcciono las salidas por pantalla a un archivo
-		PrintStream out;
-		try {
-			out = new PrintStream(new FileOutputStream("data/output.txt"));
-			System.setOut(out);
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+		if(estrategia != 1) {
+			PrintStream out;
+			try {
+				out = new PrintStream(new FileOutputStream("data/output.txt"));
+				System.setOut(out);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			}
 		}
 		ControladorTaTeTi controlador= new ControladorTaTeTi();
 		controlador.cargarTaTeTi();
-		controlador.simularPartidas(150, ControladorTaTeTi.ESTRATEGIA_RED_NEURONAL, ControladorTaTeTi.ESTRATEGIA_PROCEDURAL);
+		controlador.simularPartidas(cantPartidas, ControladorTaTeTi.ESTRATEGIA_RED_NEURONAL, estrategia);
+		
 	}
 }
